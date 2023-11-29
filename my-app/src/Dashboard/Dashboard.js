5e75895bfecb5d3ffee3c869a0be98e33812c84f
Ozana <ozanaozana58@gmail.com>
2023-11-29 12:31:39 +0200

    Added get

--- my-app/src/Dashboard/Dashboard.js
+++ my-app/src/Dashboard/Dashboard.js
@@ -1,16 +1,23 @@
-import { Alert, Button, Card, Form } from "react-bootstrap";
+import axios from "axios";
+import { Alert, Button, Card, Row, Col, Placeholder } from "react-bootstrap";
 import { useAuth } from "../Contexts/AuthProvider";
 import firebase from "../firebase";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useIdleTimer } from "react-idle-timer";
+import "./Dashboard.css";
+import logo from "../logo.svg";
 
 export default function Dashboard() {
   const [state, setState] = useState("Active");
   const [count, setCount] = useState(0);
   const [remaining, setRemaining] = useState(0);
   const { logout } = useAuth();
+  const [posts, setPosts] = useState([]);
   const navigate = useNavigate();
+  const client = axios.create({
+    baseURL: "https://jsonplaceholder.typicode.com/posts",
+  });
 
   const onIdle = () => {
     setState("Idle");
@@ -28,11 +35,16 @@ export default function Dashboard() {
     onIdle,
     onActive,
     onAction,
-    timeout: 50_000,
+    timeout: 10_000,
     throttle: 500,
   });
 
   useEffect(() => {
+    const fetchPost = async () => {
+      let response = await client.get("?_limit=10");
+      setPosts(response.data);
+    };
+    fetchPost();
     const interval = setInterval(() => {
       setRemaining(Math.ceil(getRemainingTime() / 1000));
     }, 500);
@@ -40,20 +52,44 @@ export default function Dashboard() {
     return () => {
       clearInterval(interval);
     };
-  });
+  }, []);
 
   if (state === "Idle" && firebase.auth().currentUser.email) {
     logout();
     navigate("/");
   }
   return (
-    <>
-      <h1>React Idle Timer</h1>
-      <h2>useIdleTimer</h2>
-      <br />
-      <p>Current State: {state}</p>
-      <p>Action Events: {count}</p>
-      <p>{remaining} seconds remaining</p>
-    </>
+    <div>
+      {remaining <= 5 ? (
+        <Alert variant="danger">
+          Session will be closed soon due to inactivity!
+        </Alert>
+      ) : (
+        <div />
+      )}
+
+      <div className="container-fluid d-flex justify-content-center flex-container">
+        <Row className="card-list">
+          {posts.map((post) => {
+            return (
+              <Card style={{ width: "20vw" }}>
+                <div className="overflow">
+                  <Card.Img variant="top" src={logo} />
+                </div>
+                <Card.Body>
+                  <Card.Title>{post.title}</Card.Title>
+                  <Card.Text>{post.text}</Card.Text>
+                  <Row>
+                    <Col className="m-2">
+                      <Button variant="dark">Delete</Button>
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </Row>
+      </div>
+    </div>
   );
 }
