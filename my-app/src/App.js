acaeae291a96c43251a875340177dd8160d0a656
Ozana <ozanaozana58@gmail.com>
2023-12-30 15:58:07 +0200

    incercare de XSS in comments

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,29 +7,50 @@ import Dashboard from "./Dashboard/Dashboard";
 import { AuthProvider } from "./Contexts/AuthProvider";
 
 function App() {
-    return (
-        <>
-            <div className='align-items-center justify-content-center'>
-                <div className='w-100'>
-                    <Router>
-                        <AuthProvider>
-                            <Routes>
-                                <Route path='/' element={<Login/>} />
-                                <Route
-                                    path='/signup'
-                                    element={<Signup/>}
-                                />
-                                <Route
-                                    path='/dashboard'
-                                    element={<Dashboard/>}
-                                /> 
-                            </Routes>
-                        </AuthProvider>
-                    </Router>
-                </div>
-            </div>
-        </>
-    );
+  const defaultValue = "<img onError=alert('Hacked.') src='invalid.url.com'>";
+  const [value, setValue] = React.useState(defaultValue);
+  const divRef = React.useRef(null);
+  const illegalStrings = ["<", ">", "script", "link", "img", "onError"];
+
+  return (
+    <>
+      <div className="align-items-center justify-content-center">
+        <div className="w-100">
+          <Router>
+            <AuthProvider>
+              <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+              </Routes>
+            </AuthProvider>
+          </Router>
+        </div>
+      </div>
+
+      {/* verificare a inputului sa nu permita unele cazuri de XSS folosing html tags sau script sau style tags */}
+      {/* <div className="App">
+        <textarea
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        ></textarea>
+        <div>
+          <button
+            onClick={() => {
+              let validInput = true;
+              for (let illegalString of illegalStrings) {
+                validInput = value.includes(illegalString) ? false : true;
+              }
+              if (validInput) divRef.current.innerHTML = value;
+            }}
+          >
+            Send
+          </button>
+        </div>
+        <div ref={divRef}></div>
+      </div> */}
+    </>
+  );
 }
 
 export default App;
